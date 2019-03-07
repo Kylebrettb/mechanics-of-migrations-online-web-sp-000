eba22ffced37f7117ce16be64381de1bacb20df9
Kyle  Beckstead <kyle21beckstead@gmail.com>
2019-03-07 16:11:14 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_favorite_food_to_artists.rb
+++
@@ -1,7 +0,0 @@
-# db/migrate/02_add_favorite_food_to_artists.rb
-
-class AddFavoriteFoodToArtists < ActiveRecord::Migration
-  def change
-    add_column :artists, :favorite_food, :string
-  end
-end
